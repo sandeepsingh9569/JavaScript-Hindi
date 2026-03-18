cf7d22788f9fde21df7d453c8ea1edf8215fe79e
[Sandeep Singh] <[sandeepgonda321@gamil.com]>
2026-03-18 22:20:16 +0530

    03_objects.js & 04_objects.js

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,38 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Sandeep",
+    "full name": "Sandeep Singh",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Gonda",
+    email: "sandeep@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "sandeep@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "sandeep@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
